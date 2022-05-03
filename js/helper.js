2990cc7039834764ca04b5758f2b689bccf96f0b
Fin <fantomma91@gmail.com>
2022-05-03 23:07:38 +0700

    123

---
+++ js/helper.js
@@ -0,0 +1,87 @@
+import Cookies from "js-cookie";
+import { UI_ELEMENTS } from "./uiElements";
+
+export const clearForm = (e) => e.target.closest(".form").reset();
+
+export function getValue(e) {
+  const input = e.target.querySelector(".form__input");
+  const { value } = input;
+  if (!value) throw new Error("Введите что-то");
+  return value.trim();
+}
+
+export function getToken() {
+  const token = Cookies.get("token");
+  if (!token) throw new Error("Вы не авторизованны");
+  return token;
+}
+
+export function scrollToBottom() {
+  const element = UI_ELEMENTS.MESSAGES.firstElementChild;
+  const options = { block: "end", behavior: "smooth" };
+
+  element.scrollIntoView(options);
+}
+
+export function showArrowOnScroll() {
+  const isNotStart = (-UI_ELEMENTS.MESSAGES.scrollTop > UI_ELEMENTS.MESSAGES.offsetHeight);
+  UI_ELEMENTS.START.style.opacity = isNotStart ? "1" : null;
+  UI_ELEMENTS.START.style.visibility = isNotStart ? "visible" : null;
+}
+
+export function setCookiesToken(token) {
+  const lifetime = new Date(new Date().getTime() + 3600 * 10000);
+  Cookies.set("token", token, { expires: lifetime });
+}
+
+export function showMessage(e, text) {
+  const form = e.target;
+  const formError = form.querySelector(".alert");
+  form.classList.add("note");
+  formError.textContent = text;
+}
+
+export function hideMessage(e) {
+  const form = e.target;
+  const formError = form.querySelector(".alert");
+  if (!formError) return;
+  form.classList.remove("note");
+}
+
+export function throttle(func, ms) {
+  let stopper = false;
+  let thisFunc;
+  let thisArguments;
+
+  function wrapper(...args) {
+    if (stopper) {
+      thisFunc = this;
+      thisArguments = args;
+      return;
+    }
+
+    stopper = true;
+    func.apply(this, args);
+
+    setTimeout(() => {
+      stopper = false;
+      if (thisArguments) {
+        wrapper.apply(thisFunc, thisArguments);
+        thisFunc = null;
+        thisArguments = null;
+      }
+    }, ms);
+  }
+  return wrapper;
+}
+
+export function debounce(func, ms) {
+  let timer;
+
+  return (...args) => {
+    const funcCall = () => func.apply(this, args);
+    clearTimeout(timer);
+
+    timer = setTimeout(funcCall, ms);
+  };
+}
